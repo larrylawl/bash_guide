68797fe859a170e6b323d5b3a34815d3373024d4
Larry Law <larrylaw97@gmail.com>
2019-06-02 22:50:34 +0800

    c2

---
+++ c2/chapterTwoScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Display path to home directory
+echo ~
+
+# Display terminal type
+echo $TERM
+
+explanation='Terminal type specifies how your computer and the host 
+computer (server) to which you are connected to exchange info'
+
+echo $explanation 
